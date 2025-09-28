3efda8e50012b928516b70d987749f7220046c9c
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-09-28 18:20:57 -0300

    feat: initialize python project

--- apps/ui/src/app/api/ai/route.ts
+++ apps/ui/src/app/api/ai/route.ts
@@ -9,8 +9,19 @@ export async function POST(req: NextRequest) {
   if (!parsed.success) {
     return NextResponse.json({ error: 'Invalid body' }, { status: 400 });
   }
+  const response = await fetch(process.env.AI_API || "", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: parsed.data.message,
+      }),
+    });
+
+    const externalData = await response.json();
   return NextResponse.json(
-    { ok: true, message: "teste" },
+    { ok: true, message: externalData },
     { status: 201 }
   );
 }
